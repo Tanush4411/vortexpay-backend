d412831a3ef51873c996523d8981a5ebc3036152
Tanush Sharma <chrizlove11111.com>
2026-08-04 15:42:56 +0530

    feat: initialize spring boot essentials and postgres docker service

---
+++ src/main/java/com/chrizlove/vortexpay/VortexPayApplication.java
@@ -0,0 +1,13 @@
+package com.chrizlove.vortexpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VortexPayApplication.class, args);
+    }
+
+}
